6c2110cdafee1e9475dbfb98c546a03b56ed473c
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-07-31 19:19:48 -0300

    init test

--- src/test/java/io/github/ngraciano/locadora/model/CarroTest.java
+++ src/test/java/io/github/ngraciano/locadora/model/CarroTest.java
@@ -1,4 +1,19 @@
 package io.github.ngraciano.locadora.model;
 
-public class CarroTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarroTest {
+
+    @Test
+    @DisplayName("Deve calcular o valor do aluguel corretamente.")
+    void deveCalcularValorAluguel(){
+        Carro carro=new Carro("Sedan",100.0);
+        double total=carro.calcularValorAluguel(3);
+        Assertions.assertEquals(300.0,total);
+    }
+
+
+
 }
