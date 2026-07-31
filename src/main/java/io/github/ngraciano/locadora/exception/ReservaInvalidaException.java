10a534200a169c8dd570ea82f81e8f600e1f9c49
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-07-31 19:58:25 -0300

    add ReservaInvalidaException

---
+++ src/main/java/io/github/ngraciano/locadora/exception/ReservaInvalidaException.java
@@ -0,0 +1,8 @@
+package io.github.ngraciano.locadora.exception;
+
+public class ReservaInvalidaException extends RuntimeException{
+
+    public ReservaInvalidaException(String msg){
+        super(msg);
+    }
+}
