7c267779d7577e44a6663fe69bd3174639b955b8
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-02 13:11:07 -0300

    add findByModelo method

--- src/main/java/io/github/ngraciano/locadora/repository/CarroRepository.java
+++ src/main/java/io/github/ngraciano/locadora/repository/CarroRepository.java
@@ -3,5 +3,9 @@ package io.github.ngraciano.locadora.repository;
 import io.github.ngraciano.locadora.entity.CarroEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CarroRepository extends JpaRepository<CarroEntity,Long> {
+
+    List<CarroEntity> findByModelo(String modelo);
 }
