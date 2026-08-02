801080650a6a804efee885881dbfbcbf4b6393ff
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-08-01 23:24:38 -0300

    carroEntity created

---
+++ src/main/java/io/github/ngraciano/locadora/entity/CarroEntity.java
@@ -0,0 +1,46 @@
+package io.github.ngraciano.locadora.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="carro")
+public class CarroEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String modelo;
+    private double valorDiaria;
+
+    public CarroEntity() {
+    }
+
+    public CarroEntity(String modelo, double valorDiaria) {
+        this.modelo = modelo;
+        this.valorDiaria = valorDiaria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getValorDiaria() {
+        return valorDiaria;
+    }
+
+    public void setValorDiaria(double valorDiaria) {
+        this.valorDiaria = valorDiaria;
+    }
+}
